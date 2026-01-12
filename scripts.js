545a3f90a7c163fda91921dc0e158bf1911ecb5d
Oleg Ponomarjov <erep4@inbox.lv>
2026-01-12 23:14:17 +0200

    js code fix

--- scripts.js
+++ scripts.js
@@ -3,25 +3,36 @@ const questions = [
     category: "Science",
     question: "What is the chemical symbol for gold?",
     choices: ["Go", "Gd", "Au", "Ag"],
-    answer: "Au"
+    answer: "Au",
   },
   {
     category: "Geography",
     question: "Which is the largest ocean on Earth?",
-    choices: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
-    answer: "Pacific Ocean"
+    choices: [
+      "Atlantic Ocean",
+      "Indian Ocean",
+      "Arctic Ocean",
+      "Pacific Ocean",
+    ],
+    answer: "Pacific Ocean",
   },
   {
     category: "History",
     question: "In which year did World War II end?",
     choices: ["1943", "1945", "1947", "1950"],
-    answer: "1945"
+    answer: "1945",
   },
   {
     category: "Entertainment",
-    question: "Who played the character Tony Stark in the Marvel Cinematic Universe?",
-    choices: ["Chris Evans", "Robert Downey Jr.", "Chris Hemsworth", "Mark Ruffalo"],
-    answer: "Robert Downey Jr."
+    question:
+      "Who played the character Tony Stark in the Marvel Cinematic Universe?",
+    choices: [
+      "Chris Evans",
+      "Robert Downey Jr.",
+      "Chris Hemsworth",
+      "Mark Ruffalo",
+    ],
+    answer: "Robert Downey Jr.",
   },
   {
     category: "Technology",
@@ -30,79 +41,30 @@ const questions = [
       "Hyper Trainer Marking Language",
       "Hyper Text Marketing Language",
       "Hyper Text Markup Language",
-      "Hyper Transfer Markup Protocol"
+      "Hyper Transfer Markup Protocol",
     ],
-    answer: "Hyper Text Markup Language"
-  }
+    answer: "Hyper Text Markup Language",
+  },
 ];
 
-const getRandomQuestion = arr => {
-  return arr[Math.floor(Math.random() * arr.length)].question;
-}
+const getRandomQuestion = (question) => {
+  const random = Math.floor(Math.random() * question.length);
+  return question[random];
+};
 
-// код на freeCodeCamp
+let question = getRandomQuestion(questions);
 
-const questions = [
-  {
-    category: "Science",
-    question: "What is the chemical symbol for gold?",
-    choices: ["Go", "Gd", "Au"],
-    answer: "Au"
-  },
-  {
-    category: "Geography",
-    question: "Which is the largest ocean on Earth?",
-    choices: ["Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
-    answer: "Pacific Ocean"
-  },
-  {
-    category: "History",
-    question: "In which year did World War II end?",
-    choices: ["1945", "1947", "1950"],
-    answer: "1945"
-  },
-  {
-    category: "Entertainment",
-    question: "Who played the character Tony Stark in the Marvel Cinematic Universe?",
-    choices: ["Robert Downey Jr.", "Chris Hemsworth", "Mark Ruffalo"],
-    answer: "Robert Downey Jr."
-  },
-  {
-    category: "Technology",
-    question: "What does 'HTML' stand for?",
-    choices: [
-      "Hyper Text Marketing Language",
-      "Hyper Text Markup Language",
-      "Hyper Transfer Markup Protocol"
-    ],
-    answer: "Hyper Text Markup Language"
-  }
-];
-
-const getRandomQuestion = arr => {
-  let random = Math.floor(Math.random() * arr.length)
-  const que = arr[random];
-   return que;
-} 
-
-const getRandomComputerChoice = arr => {
-  let question = getRandomQuestion(arr)
-  let random = question.choices[Math.floor(Math.random() * question.choices.length)];
-  return {question, random}
-}
-
+const getRandomComputerChoice = (choice) => {
+  const random = Math.floor(Math.random() * choice.choices.length);
+  return choice.choices[random];
+};
+let choice = getRandomComputerChoice(question);
 
 const getResults = (question, choice) => {
-  question = question.question;
-  
-  choice = question.choices
-  if (question.answer === choice){
-    return "The computer's choice is correct!"
+  if (choice === question.answer) {
+    return "The computer's choice is correct!";
   } else {
-    return `The computer's choice is wrong. The correct answer is: ${question.answer}`
+    return `The computer's choice is wrong. The correct answer is: ${question.answer}`;
   }
-}
- console.log(getResults(questions))
-// console.log(getRandomQuestion(questions))
-// console.log(getRandomComputerChoice(questions))
-// console.log(getResults(getRandomQuestion(questions), getRandomComputerChoice(questions)))
+};
+console.log(getResults(question, choice));
